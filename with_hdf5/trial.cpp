fd8fd6b30e2ac9ee6710833dcbb4e76d2e58958b
Owen Arnold <owen.arnold@stfc.ac.uk>
2014-11-04 09:37:15 +0000

    reading data via hdf5

--- with_hdf5/trial.cpp
+++ with_hdf5/trial.cpp
@@ -1,33 +1,60 @@
 #include <hdf5.h>
 #include <string>
+#include <iostream>
+#include <numeric>
 #include "timer.h"
 #include "Poco/Path.h"
+#include "Timer.h"
+
+int multiply(int x, int y)
+{
+  return x * y;
+}
+
+std::vector<double> read_double_data(hid_t file_id, const std::string& path)
+{
+  hid_t dataset_id = H5Dopen(file_id, path.c_str(), H5P_DEFAULT);
+  hid_t dataspace = H5Dget_space(dataset_id); /* dataspace handle */
+  int rank = H5Sget_simple_extent_ndims(dataspace);
+  std::vector<hsize_t> dims_out(rank);
+  int status_n = H5Sget_simple_extent_dims(dataspace, &dims_out[0], NULL);
+
+  int product = std::accumulate(dims_out.begin(), dims_out.end(), 1, multiply);
+
+  std::vector<double> data(product);
+  herr_t status = H5Dread(dataset_id, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, &data[0]);
+  H5Dclose(dataset_id);
+  return data;
+}
 
 int main()
 {
+  common::Timer timer;
+  timer.start();
+
   Poco::Path file_path("../common/INTER00013460.nxs");
   const std::string file = file_path.toString();
 
-  hid_t       file_id, dataset_id;  /* identifiers */
-  herr_t      status;
-  int         i, j, dset_data[4][6];
-
-
   /* Open an existing file. */
-  file_id = H5Fopen(file.c_str(), H5F_ACC_RDWR, H5P_DEFAULT);
+  hid_t file_id = H5Fopen(file.c_str(), H5F_ACC_RDWR, H5P_DEFAULT);
 
+  hid_t dataset_id1 = H5Gopen(file_id, "/mantid_workspace_1", H5P_DEFAULT);
+  hid_t dataset_id2 = H5Gopen(file_id, "/mantid_workspace_1/workspace", H5P_DEFAULT);
 
-  H5Gopen(file_id, "/mantid_workspace_1", H5P_DEFAULT);
-  //H5Dopen(group, "CData", H5P_DEFAULT);
+  std::string path = "/mantid_workspace_1/workspace/values";
+  std::vector<double> values = read_double_data(file_id, path);
 
-  /* Open an existing dataset. */
-  //dataset_id = H5Dopen2(file_id, "/mantid_workspace_1", H5P_DEFAULT);
-  //status = H5Dread(dataset_id, H5T_NATIVE_INT, H5S_ALL, H5S_ALL, H5P_DEFAULT, dset_data);
+  path = "/mantid_workspace_1/workspace/errors";
+  std::vector<double> errors = read_double_data(file_id, path);
 
-  /* Close the dataset. */
-  //status = H5Dclose(dataset_id);
+  H5Gclose(dataset_id1);
+  H5Gclose(dataset_id2);
 
-  /* Close the file. */
-  status = H5Fclose(file_id);
+  herr_t status = H5Fclose(file_id);
+
+  timer.stop();
+
+  std::cout << "Elapsed time in ms: " << timer.elapsed_ms() << std::endl;
+
   return 0;
 }
